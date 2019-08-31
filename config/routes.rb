58a59cd76173be1e0b03b93481757c51609f9043
Ivan Filimonov <snake73w@gmail.com>
2019-08-31 18:06:31 +0400

    add web boards and sessions controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  namespace :web do
-    get 'boards/show'
+  root to: 'web/board#show'
+
+  scope module: :web do
+    resource :board, only: :show
+    resource :session, only: :new
   end
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
